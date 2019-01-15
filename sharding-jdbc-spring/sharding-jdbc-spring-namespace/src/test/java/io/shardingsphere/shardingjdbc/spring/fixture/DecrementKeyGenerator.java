6f3f1a7042046059e4c739fa8f6f6fc8fcf62b65
tristaZero <hannkenn@163.com>
2019-01-15 15:52:33 +0800

    MODIFY DecrementKeyGenerator

--- sharding-jdbc-spring/sharding-jdbc-spring-namespace/src/test/java/io/shardingsphere/shardingjdbc/spring/fixture/DecrementKeyGenerator.java
+++ sharding-jdbc-spring/sharding-jdbc-spring-namespace/src/test/java/io/shardingsphere/shardingjdbc/spring/fixture/DecrementKeyGenerator.java
@@ -17,16 +17,27 @@
 
 package io.shardingsphere.shardingjdbc.spring.fixture;
 
-import io.shardingsphere.core.keygen.KeyGenerator;
 
+import io.shardingsphere.core.keygen.generator.KeyGenerator;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Properties;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public final class DecrementKeyGenerator implements KeyGenerator {
     
     private final AtomicInteger sequence = new AtomicInteger(100);
     
+    @Getter
+    @Setter
+    private Properties properties = new Properties();
+    
     @Override
-    public Number generateKey() {
+    public Comparable<?> generateKey() {
         return sequence.decrementAndGet();
     }
+    
+    @Override
+    public String getType() { return "DECREMENT"; }
 }
